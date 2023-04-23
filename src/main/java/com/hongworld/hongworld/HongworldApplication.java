84d2691ee3dddbd97b918fdcc4ea7a93027eea1d
TAEHONG <hong2wrd@gmail.com>
2023-04-23 09:28:48 +0900

    initial commit

---
+++ src/main/java/com/hongworld/hongworld/HongworldApplication.java
@@ -0,0 +1,13 @@
+package com.hongworld.hongworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongworld/hongworld/HongworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongworld.hongworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
